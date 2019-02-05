295949c4b0191007121a1ca9720f225706ae49cb
amoham37 <amoham811@gmail.com>
2019-02-04 20:15:20 -0600

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Ahmet Mohammed on 2/4/19.
+//  Copyright © 2019 Ahmet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
